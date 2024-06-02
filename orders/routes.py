ce33f8c9f72f40f7fc7bd77a330efc745075044f
shmuli <shmulikeller@gmail.com>
2024-06-02 05:01:45 -0400

    moved_code_out_orders_module_to_make_work_on_server

--- orders/routes.py
+++ orders/routes.py
@@ -5,27 +5,9 @@ from .utils import timeStringToInt
 
 orders_bp = Blueprint('orders', __name__)
 
-@orders_bp.route("/orders/<int:id>", methods=["GET"])
-def get_order(id):
-    return jsonify(database_interface.get_order(id))
+# @orders_bp.route("/orders/<int:id>", methods=["GET"])
+# def get_order(id):
+#     return jsonify(database_interface.get_order(id))
 
 
 
-@orders_bp.route("/create", methods=["POST"])
-def create_order_post():
-    name = request.form.get("name")
-    pickUpTime = request.form.get("pickUpTime")
-    items = request.form.get("items")
-    phone = request.form.get("phone")
-    pickUpTimeInt = timeStringToInt(pickUpTime)
-    if items[0] == "'" or items[0] == '"':
-        items = items[1:-1]
-    if items[-1] == "'" or items[-1] == '"':
-        items = items[:-1]
-    print(f"{name = }, {pickUpTime = }, {pickUpTimeInt = }, {items = } {phone = }")
-    return jsonify(database_interface.create_order(name, pickUpTime, pickUpTimeInt, items, phone))
-
-@orders_bp.route("/all", methods=["GET"])
-def get_all_orders():
-    allorders = database_interface.get_all_orders()
-    return jsonify(allorders)
