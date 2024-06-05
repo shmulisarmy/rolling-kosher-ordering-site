88baaf7f29227e92710fe38933039561ac6b60ec
shmuli <shmulikeller@gmail.com>
2024-06-04 23:45:34 -0400

    removed_server_code

--- orders/utils.py
+++
@@ -1,9 +0,0 @@
-def timeStringToInt(time: str) -> int:
-    hour, minute = time.split(':')
-    return int(hour) * 60 + int(minute)
-
-
-def intToTimeString(time: int) -> str:
-    hour = time // 60
-    minute = time % 60
-    return f"{hour:02d}:{minute:02d}"
