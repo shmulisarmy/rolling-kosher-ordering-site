4d6aa1b742ae64457ef00e1186ef62a00ee744a9
shmuli <shmulikeller@gmail.com>
2024-06-05 11:48:56 -0400

    renamed_flask_app_to_main

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import Flask, jsonify
 from flask.templating import render_template
 from flask_cors import CORS
 from orders.routes import orders_bp
+from orders import database_interface
+from orders.utils import timeStringToInt
 
 
 app = Flask(__name__)
@@ -10,7 +12,24 @@ app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'your secret key'
 
 
-app.register_blueprint(orders_bp, url_prefix='/orders')
+# app.register_blueprint(orders_bp, url_prefix='/orders')
+
+
+
+
+@app.route("/orders/create", methods=["POST"])
+def create_order_post():
+    name = request.form.get("name")
+    pickUpTime = request.form.get("pickUpTime")
+    items = request.form.get("items")
+    phone = request.form.get("phone")
+    pickUpTimeInt = timeStringToInt(pickUpTime)
+    if items[0] == "'" or items[0] == '"':
+        items = items[1:-1]
+    if items[-1] == "'" or items[-1] == '"':
+        items = items[:-1]
+    print(f"{name = }, {pickUpTime = }, {pickUpTimeInt = }, {items = } {phone = }")
+    return jsonify(database_interface.create_order(name, pickUpTime, pickUpTimeInt, items, phone))
 
 
 @app.route("/orders/create", methods=["GET"])
@@ -23,14 +42,15 @@ def index():
     return render_template('index.html', title='Home')
 
 
-# @app.after_request
-# def add_header(response):
-#     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-#     response.headers['Cache-Control'] = 'public, max-age=0'
-#     return response
+
+@app.route("/all")
+def get_all_orders():
+    allorders = database_interface.get_all_orders()
+    return jsonify(allorders)
+
+
+
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
-
-#run command: uvicorn main:app --reload
