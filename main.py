071b9198b334c565e18cfd8febcb45bc7b645380
shmuli <shmulikeller@gmail.com>
2024-06-05 12:08:45 -0400

    added_log_file

--- main.py
+++ main.py
@@ -19,22 +19,28 @@ app.config['SECRET_KEY'] = 'your secret key'
 
 @app.route("/orders/create", methods=["POST"])
 def create_order_post():
-    name = request.form.get("name")
-    pickUpTime = request.form.get("pickUpTime")
-    items = request.form.get("items")
-    phone = request.form.get("phone")
-    pickUpTimeInt = timeStringToInt(pickUpTime)
-    if items[0] == "'" or items[0] == '"':
-        items = items[1:-1]
-    if items[-1] == "'" or items[-1] == '"':
-        items = items[:-1]
-    print(f"{name = }, {pickUpTime = }, {pickUpTimeInt = }, {items = } {phone = }")
-    return jsonify(database_interface.create_order(name, pickUpTime, pickUpTimeInt, items, phone))
+    try:
+        name = request.form.get("name")
+        pickUpTime = request.form.get("pickUpTime")
+        items = request.form.get("items")
+        phone = request.form.get("phone")
+        pickUpTimeInt = timeStringToInt(pickUpTime)
+        if items[0] == "'" or items[0] == '"':
+            items = items[1:-1]
+        if items[-1] == "'" or items[-1] == '"':
+            items = items[:-1]
+        print(f"{name = }, {pickUpTime = }, {pickUpTimeInt = }, {items = } {phone = }")
+        return jsonify(database_interface.create_order(name, pickUpTime, pickUpTimeInt, items, phone))
+    except Exception as e:
+        write_to_file(str(e))
 
 
 @app.route("/orders/create", methods=["GET"])
 def create_order_get():
-    return render_template("create.html")
+    try:
+        return render_template("create.html")
+    except Exception as e:
+        write_to_file(str(e))
 
 
 @app.route('/')
@@ -45,11 +51,19 @@ def index():
 
 @app.route("/all")
 def get_all_orders():
-    allorders = database_interface.get_all_orders()
-    return jsonify(allorders)
+    try:
+        allorders = database_interface.get_all_orders()
+        return jsonify(allorders)
+    except Exception as e:
+        write_to_file(str(e))
 
 
 
+
+
+def write_to_file(data):
+    with open('data.json', 'w') as f:
+        f.write(data)
 
 
 if __name__ == '__main__':
